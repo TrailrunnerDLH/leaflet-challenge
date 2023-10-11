f99e0a86fb3628bea98ffd8cfd0d17d2f97d419c
TrailrunnerDLH <129078484+TrailrunnerDLH@users.noreply.github.com>
2023-10-10 21:43:34 -0500

    Base tile and leaflet map

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -2,8 +2,8 @@
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
 
 // Use D3 library to fetch data
-d3.json(url).then(function (data) { 
-    
+d3.json(url).then(function (data) {
+
     let long = data.features[0].geometry.coordinates[0];
     console.log(long);
     let lat = data.features[0].geometry.coordinates[1]
@@ -12,9 +12,19 @@ d3.json(url).then(function (data) {
     console.log(depth);
     let mag = data.features[0].properties.mag;
     console.log(mag);
-    
-    
+
 
     console.log(data.features[1]);
-  
-  });
+
+
+    //Create base Leaflet map
+    let myMap = L.map("map", {
+        center: [40.7128, -74.0059],
+        zoom: 11
+    })
+    //Create base tile layer
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreeMap</a> contributors'
+    }).addTo(myMap)
+
+});
