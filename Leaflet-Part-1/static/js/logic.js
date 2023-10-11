870f0af783e3078a525727d3470b9857a65106a9
TrailrunnerDLH <129078484+TrailrunnerDLH@users.noreply.github.com>
2023-10-10 22:00:56 -0500

    Added earthquake markers to map

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,6 +1,12 @@
 // Store json url in variable
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
 
+function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
+        "<hr> <p> Earthquake Magnitude: " + feature.properties.mag + "</p>")
+}
+
 // Use D3 library to fetch data
 d3.json(url).then(function (data) {
 
@@ -16,11 +22,19 @@ d3.json(url).then(function (data) {
 
     console.log(data.features[1]);
 
+    var earthquakes = L.geoJSON(data.features, {
+        pointToLayer: function (feature, coordinates) {
+            // console.log(feature)
+            console.log(coordinates);
+            return L.circle(coordinates);
+        }
+    })
 
     //Create base Leaflet map
     let myMap = L.map("map", {
         center: [40.7128, -74.0059],
-        zoom: 11
+        zoom: 2,
+        layers: [earthquakes]
     })
     //Create base tile layer
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
